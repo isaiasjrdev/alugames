f4fa20c8cfc012adfcd35b522c61d60023e278e8
Isaias Junior <isaiscastro56@gmail.com>
2024-06-29 18:41:21 -0300

    recuperando os elementos

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function alterarStatus(id) {
+   let gameClicaco = document.getElementById(`game-${id}`);
+   let imagem = gameClicaco.querySelector('.dashboard__item__img');
+   let botao = gameClicaco.querySelector('.dashboard__item__button');
+   let nomeJogo = gameClicaco.querySelector('.dashboard__item__name');
+
+}
